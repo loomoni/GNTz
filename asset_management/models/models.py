d24e5bfe2cf797d65adc8030daad26b72f90eb60
loomoni <loomonimorwo1@gmail.com>
2023-09-25 15:09:11 +0300

    asset report date filter range added

--- asset_management/models/models.py
+++ asset_management/models/models.py
@@ -654,8 +654,13 @@ class AssetListWizard(models.TransientModel):
         worksheet.write('I8:I8', 'Status', cell_text_sub_title_format)
 
         department_asset = self.env['account.asset.asset'].sudo().search(
-            [('department_id', '=', self.department_name)])
-        all_asset = self.env['account.asset.asset'].sudo().search([])
+            [('department_id', '=', self.department_name), ('date', '>=', self.date_from),
+             ('date', '<=', self.date_to)])
+        # all_asset = self.env['account.asset.asset'].sudo().search([])
+        all_asset = self.env['account.asset.asset'].sudo().search([
+            ('date', '>=', self.date_from),
+            ('date', '<=', self.date_to)
+        ])
 
         row = 8
         col = 0
