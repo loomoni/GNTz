7be53be1c8895a60c92e6013297fc92c61d654b8
loomoni <loomonimorwo1@gmail.com>
2025-05-08 19:29:19 +0300

    zero valued not pushed

--- custom_inventory/models/models.py
+++ custom_inventory/models/models.py
@@ -303,7 +303,7 @@ class InventoryStockOut(models.Model):
     @api.onchange('line.balance_stock', 'line.issued_quantity')
     def button_checked(self):
         for line in self.line_ids:
-            if line.issued_quantity <= -1:
+            if line.issued_quantity <= 0:
                 raise ValidationError(_("You can't issue less than 0 goods"))
             elif line.balance_stock - line.issued_quantity < 0:
                 raise ValidationError(_("There is no enough Item to issue please check stock balance"))
